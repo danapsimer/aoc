14b4c3a78cf139215b988476989631c1de2a5779
Dana P'Simer <danap@dhptech.com>
2020-12-11 18:10:03 -0500

    added visualization to 2020/day11

--- 2020/day11/day11.go
+++ 2020/day11/day11.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"bufio"
+	"github.com/gdamore/tcell/v2"
 	"io"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
+	"unicode/utf8"
 )
+
+var defStyle tcell.Style
+var screen tcell.Screen
 
 type grid []string
 
@@ -63,7 +70,7 @@ func (g grid) traverseSeats(traversFn func(dest, src grid, x, y int)) grid {
 
 func (g grid) applyRules() grid {
 	return g.traverseSeats(func(cp, g grid, x, y int) {
-		c := g.get(x,y)
+		c := g.get(x, y)
 		adjOccupiedCount := 0
 		for yd := -1; yd <= 1; yd++ {
 			for xd := -1; xd <= 1; xd++ {
@@ -82,7 +89,7 @@ func (g grid) applyRules() grid {
 
 func (g grid) applyRulesPart2() grid {
 	return g.traverseSeats(func(cp, g grid, x, y int) {
-		c := g.get(x,y)
+		c := g.get(x, y)
 		visibleOccupiedCount := 0
 		for yd := -1; yd <= 1; yd++ {
 			for xd := -1; xd <= 1; xd++ {
@@ -112,14 +119,67 @@ func (g grid) String() string {
 	return strings.Join(g, "\n")
 }
 
-func (original grid) applyRulesUntilStable(rulesFn func(g grid) grid) grid {
-	g := original
-	cp := g.applyRules()
-	for !cp.equals(g) {
-		g = cp
-		cp = rulesFn(g)
+var occupiedEmojis = []string{
+	"\U0001F64D", "\U0001F64E", "\U0001F648", "\U0001F469", "\U0001F468",
+}
+var chair = "\u2441"
+
+
+func (g grid) Draw() {
+	sw, sh := screen.Size()
+	if sw > g.width()+2 {
+		sw = g.width() + 2
 	}
-	return cp
+	if sh > g.height()+2 {
+		sh = g.height() + 2
+	}
+
+	for col := 1; col < sw; col++ {
+		screen.SetContent(col, 0, tcell.RuneHLine, nil, defStyle)
+		screen.SetContent(col, sh-1, tcell.RuneHLine, nil, defStyle)
+	}
+	for row := 1; row < sh; row++ {
+		screen.SetContent(0, row, tcell.RuneVLine, nil, defStyle)
+		screen.SetContent(sw-1, row, tcell.RuneVLine, nil, defStyle)
+	}
+	screen.SetContent(0, 0, tcell.RuneULCorner, nil, defStyle)
+	screen.SetContent(sw-1, 0, tcell.RuneURCorner, nil, defStyle)
+	screen.SetContent(0, sh-1, tcell.RuneLLCorner, nil, defStyle)
+	screen.SetContent(sw-1, sh-1, tcell.RuneLRCorner, nil, defStyle)
+	for y := range g {
+		if y < sh-2 {
+			for x := range g[y] {
+				if x < sw-2 {
+					r := rune(g[y][x])
+					switch r {
+					case '#':
+						r, _ = utf8.DecodeRuneInString(occupiedEmojis[rand.Intn(len(occupiedEmojis))])
+					case 'L':
+						r, _ = utf8.DecodeRuneInString(chair)
+					case '.':
+						r = ' '
+					}
+					screen.SetContent(x+1, y+1, r, nil, defStyle)
+				}
+			}
+		}
+	}
+	screen.Show()
+}
+
+func (original grid) applyRulesUntilStable(rulesFn func(g grid) grid) <-chan grid {
+	gchan := make(chan grid)
+	go func() {
+		g := original
+		cp := g.applyRules()
+		for !cp.equals(g) {
+			gchan <- g
+			g = cp
+			cp = rulesFn(g)
+		}
+		close(gchan)
+	}()
+	return gchan
 }
 
 func (g grid) countOccupiedSeats() int {
@@ -135,13 +195,52 @@ func (g grid) countOccupiedSeats() int {
 }
 
 func main() {
+
+	s, e := tcell.NewScreen()
+	if e != nil {
+		log.Fatalf("%v\n", e)
+		os.Exit(1)
+	}
+	if e := s.Init(); e != nil {
+		log.Fatalf("%v\n", e)
+		os.Exit(1)
+	}
+	defStyle = tcell.StyleDefault.
+		Background(tcell.ColorReset).
+		Foreground(tcell.ColorReset)
+	s.SetStyle(defStyle)
+	s.Clear()
+	screen = s
+
 	original := readGrid(os.Stdin)
-	g := original.applyRulesUntilStable(func(g grid) grid {
-		return g.applyRules()
-	})
-	log.Printf("part 1 occupied = %d", g.countOccupiedSeats())
-	g = original.applyRulesUntilStable(func(g grid) grid {
-		return g.applyRulesPart2()
-	})
-	log.Printf("part 2 occupied = %d", g.countOccupiedSeats())
+	go func() {
+		gchan := original.applyRulesUntilStable(func(g grid) grid {
+			return g.applyRules()
+		})
+		for g := range gchan {
+			g.Draw()
+			time.Sleep(time.Millisecond*500)
+		}
+
+		gchan = original.applyRulesUntilStable(func(g grid) grid {
+			return g.applyRulesPart2()
+		})
+
+		for g := range gchan {
+			g.Draw()
+			time.Sleep(time.Millisecond*500)
+		}
+	}()
+
+	for {
+		switch ev := s.PollEvent().(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape {
+				s.Fini()
+				os.Exit(0)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module aoc
 
 require (
-	github.com/gizak/termui v3.1.0+incompatible // indirect
+	github.com/gdamore/tcell/v2 v2.0.0
 	github.com/gizak/termui/v3 v3.1.0
 	github.com/stretchr/testify v1.4.0
 )
